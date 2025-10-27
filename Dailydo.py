edfd30fbc0df366e636c1bb6fa95fefade761dc7
Abdinasir Ismail <abdinasird76@gmail.com>
2025-10-27 23:45:54 +0300

    Add files via upload

---
+++ Dailydo.py
@@ -0,0 +1,31 @@
+# halkan waxaan ku diyaarinayaa timetable aan ugu talagalay inaan si maalinle ah u isticmaalo .
+
+
+print('----- TIMETABLE-----')
+print('-----------------')
+
+print('07:00am\n'
+      '12:00pm \n'
+      '03:00pm \n'
+      '09:0pm\n')
+
+
+choice = input ('TIME ! : ')
+
+if choice == '07:00am':
+    print('Your will go to the job!')
+
+elif choice == '12:00pm':
+    print('Its prayer time.')
+    
+elif choice == '03:00pm':
+    print('Its lunch time !')
+    
+elif choice == '09:00pm':
+    print('your have a class , save the time!!')
+    
+    
+    
+    
+else:
+    print('plz check the time !')
